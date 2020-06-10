a7eb54808a6f3bcdd227610e675ca8d08bac5635
Bharat Dev Burman <bharat.singh7924@gmail.com>
2020-06-10 14:15:56 +0530

    k5fpr: use correct aapt config

--- device.mk
+++ device.mk
@@ -4,13 +4,13 @@ $(call inherit-product, device/lenovo/mt6753-common/platform.mk)
 $(call inherit-product, vendor/lenovo/k5fpr-vendor.mk)
 
 ### DALVIK
-$(call inherit-product, frameworks/native/build/phone-xhdpi-3072-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 
 DEVICE_PATH := device/lenovo/k5fpr
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := 420dpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 
 DEVICE_PACKAGE_OVERLAYS += \
